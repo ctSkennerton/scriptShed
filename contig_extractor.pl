e4f0d8cad8b23951ad920b8cc4b90cb60aea9fc8
ctSkennerton <c.skennerton@gmail.com>
2012-03-29 20:52:29 +1000

    added in two new options where the input and database file heads can be split by a perl regular expression

--- contig_extractor.pl
+++ contig_extractor.pl
@@ -63,51 +63,78 @@ if($ARGV{'-f'}) {
     my @aux = undef;
     my ($name, $seq, $qual);
     while (($name, $seq, $qual) = &readfq($query, \@aux)) {
+        if($ARGV{'-Ri'}) {
+            # perform the split according to the user
+            my @p = split(/$ARGV{'-Ri'}->{separator}/, $name);
+            $name = $p[$ARGV{'-Ri'}->{field_num}];
+        }
         $seqs{$name} = 1;
     }
 } elsif (! defined $ARGV{'-n'}) {
     while (my $line = <$query>) 
     {
+        my $name = undef;
         chomp $line;
         if($ARGV{'-l'}) {
-            &list($line);
+            $name = &list($line);
         } elsif ($ARGV{'-b'}) {
-            &blast($line);
+           $name = &blast($line);
         } elsif ($ARGV{'-s'}) {
             # skip header lines
             next if $line =~ /^@/;
-            &sam($line);
+            $name =&sam($line);
         } elsif ($ARGV{'-m'}) {
-            &mannotator("UniRef90_".$line);
+            $name =&mannotator("UniRef90_".$line);
         } else {
             next if ($line =~ /^\#/);
             last if ($line =~ /\#+FASTA/i);
-            &gff($line);
+            $name =&gff($line);
+        }
+        unless(defined $name ) {
+            die "Crazy error!\n";
+        } else {
+            if($ARGV{'-Ri'}) {
+                # perform the split according to the user
+                my @p = split(/$ARGV{'-Ri'}->{separator}/, $name);
+                $name = $p[$ARGV{'-Ri'}->{field_num}];
+            }
+            $seqs{$name} = 1;
         }
     }
 } else {
     foreach my $e (@{$ARGV{'-n'}}) {
+        if($ARGV{'-Ri'}) {
+            # perform the split according to the user
+            my @p = split(/$ARGV{'-Ri'}->[0]/, $e);
+            $e = $p[$ARGV{'-Ri'}->[1]];
+        }
         $seqs{$e} = 1;
     }
 }
 close $query;
 
 my @aux = undef;
-my ($name, $seq, $qual);
+my ($name,$name2, $seq, $qual);
 foreach my $database (@{$ARGV{'-d'}}) {
     open(DB,'<',$database) or die $!;
     while (($name, $seq, $qual) = readfq(\*DB, \@aux)) 
     {
+        $name2 = $name;
+        if($ARGV{'-Rd'}) {
+            # perform the split according to the user
+            my @p = split(/$ARGV{'-Rd'}->{separator_d}/, $name);
+            $name = $p[$ARGV{'-Rd'}->{field_num_d}];
+        }
         if (exists $seqs{$name})
         {
             unless($ARGV{'-v'})
             {
-                print_seq(\$name,\$seq,\$qual, $outfile);
+                print_seq(\$name2,\$seq,\$qual, $outfile);
             }
         }
         elsif ($ARGV{'-v'})
         {
-                print_seq(\$name,\$seq,\$qual, $outfile);
+                print_seq(\$name2,\$seq,\$qual, $outfile);
         }
     }
     close DB;
@@ -171,7 +198,7 @@ sub readfq {
 
 sub list{
   	my ($line) = shift;
-	$seqs{$line} = 1;
+	return $line;
 }
 
 sub blast{
@@ -179,11 +206,11 @@ sub blast{
   my @columns = split(/\t/, $line);
   	if (exists $ARGV{'-S'})
 	{
-		$seqs{$columns[1]} = $columns[0];
+		return $columns[1];
 	}
 	else
 	{
-		$seqs{$columns[0]} = $columns[1];
+		return $columns[0];
 	}
 }
 
@@ -196,21 +223,21 @@ sub sam{
         # test whether the read is paired
         if (($c[1] & 1 ) && ($c[1] & 128)) {
             # test whether the read is the second pair
-            $seqs{$c[0]."/2"} = 1;
+            return $c[0]."/2";
         } else {
-            $seqs{$c[0]."/1"} = 1;
+            return $c[0]."/1";
         }
     }
 }
 sub gff {
     my ($line) = shift;
     my @c = split(/\t/, $line);
-    $seqs{$c[0]} = 1;
+    return $c[0];
 }
 sub mannotator{
     my ($line) = shift;
     my @columns = split /\^/, $line;
-    $seqs{$columns[0]} = 1;
+    return $columns[0];
 }
 
 #sub checkParams 
@@ -353,6 +380,25 @@ A list of sequence names to extract in the form of a space separated list
 =item -v
 
 Invert the match. ie extract non-matching reads
+
+=item -Ri <separator> <field_num>
+
+The header in the input file contains additional information that must be removed.
+This option takes two arguements, a separator, that can be any valid perl regular expression
+to split the input identifier and the field to be used as the key for extraction (zero indexed).
+
+=for Euclid
+    field_num.type: i
+    field_num.type.error: "Please specify an integer for the field_num"
+
+=item -Rd <separator_d> <field_num_d>
+
+The header in the database file contains additional information that can be removed.
+Specify the options the same as option -Ri above
+
+=for Euclid
+    field_num_d.type: i
+    field_num_d.type.error: "Please specify an integer for the field number"
 
 =back
 
